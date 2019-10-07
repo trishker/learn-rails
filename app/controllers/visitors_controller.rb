31ca863d52e4908ec68d3f2cb3d6c7f399646871
trishker <kernantrish@gmail.com>
2019-10-07 17:44:26 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
 # this is the hidden code
 #    render 'visitors/new'
   end
