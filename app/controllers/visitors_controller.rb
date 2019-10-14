564cbe96fb35179054fbfebe1dd3c8459ad6af97
trishker <kernantrish@gmail.com>
2019-10-14 12:42:38 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,10 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
-# this is the hidden code
-#    render 'visitors/new'
   end
 
 end
